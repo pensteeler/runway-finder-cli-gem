000a89c56aa9eafd274b1f4e8af82ff1ebf5ff5b
John Vascsinec <pensteeler@gmail.com>
2017-07-01 01:25:18 -0500

    Add files via upload
    Add functionality to list out airports.

--- lib/runway_finder/airport.rb
+++ lib/runway_finder/airport.rb
@@ -1,5 +1,7 @@
 class RunwayFinder::Airport
 
+  @@all = []
+
   attr_accessor :name, :code, :distance, :url, :runway
 
   def self.nearby( zip_code )
@@ -33,46 +35,50 @@ class RunwayFinder::Airport
   end
 
   def self.scrape_by_zipcode( url_with_zip )
+
+    airports = []
+
     doc = Nokogiri::HTML( open(url_with_zip) )
 
-    all = doc.css("div#list tr td")
+    all = doc.css("div#list tr")
     puts "\n*************************"
     puts "All:#{all}"
-    puts "*************************\n"
-    all.each.with_index(1) do |row, i|
-      puts "\nRow #{i}"
-      number = row.css("td")[0]
-      puts "Number:#{number}"
+    puts "***********  After ALL **************\n"
+    # This has all the codes together
+    #code = doc.css(".loaddetails")[0].text
 
-      code = doc.css(".loaddetails").text
-      puts "Code is:#{code}"
+    doc.css("div#list tr").each_with_index do |row,i|
+      puts "\n** i:#{i} **\n"
+      if i > 0 && row.css("td")[0].text != "" && i < 20
+        puts "\nRow: #{row}:**END OF ROW**"
+        number = row.css("td")[0].text
 
-      distance = row.css("td")[2].text
-      puts "Distance:#{distance}"
+        code = row.css("td")[1].text
+        code_link = doc.css(".loaddetails").text
+        distance = row.css("td")[2].text
+        name = row.css("td")[3].text
 
-      manager = row.css("td title")[3]
-      puts "Manager:#{manager}"
+        puts "Number:#{number}"
+        puts "Code is:#{code}"
+        puts "Distance:#{distance}"
+        puts "Name:#{name}"
 
-      name = row.css("td")[3].text
-      puts "Name:#{name}"
+        airports << { number: number, code: code, distance: distance, name: name }
+      end # if
+    end #doc.css("div#list tr").each_with_index do |row,i|
 
-      distance = row.css("div#list td")[2].text
+    puts airports
+
+    airports.each do |a|
+      a.each do |k,v|
+      #puts "#{number.v}. is #{distance.v} miles away from #{name.v}(#{code.v}.)"
+        puts "#{v}"
+      end
     end
 
-    code = doc.css(".loaddetails").text
-    distance = doc.css("td")[2].text
-    manager = doc.css("td title")[3]
-    name = doc.css("td")[3].text
+    #codeLink = doc.css(".loaddetails").text
 
-    distance = doc.css("div#list td")[2].text
-
-    puts "\n*** AFTER  ***"
-    puts "Number:#{number}"
-    puts "Code is:#{code}"
-    puts "Distance:#{distance}"
-    puts "Manager:#{manager}"
-    puts "Name:#{name}"
-    puts "\n*** AFTER  ***"
+    #distance = doc.css("div#list td")[2].text
 
 
     # Site will display "Sorry, we could not find any suitable airports for this address!"
@@ -104,7 +110,7 @@ class RunwayFinder::Airport
     # =>   :date=>"06 Jan 2010",
     # =>   :views=>"1,320",
     # =>   :name=>"Paul M"}]
-  end
+  end # scrape_by_zipcode
 
 
-end
+end #class RunwayFinder::Airport
