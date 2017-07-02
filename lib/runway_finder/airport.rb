9baacd9e29d2d85ead6578aafc6645f98a475b31
John Vascsinec <pensteeler@gmail.com>
2017-07-01 19:28:55 -0500

    Add files via upload
    Working Version 1.0

--- lib/runway_finder/airport.rb
+++ lib/runway_finder/airport.rb
@@ -25,10 +25,10 @@ class RunwayFinder::Airport
 
     if airports != nil
       puts "These airports are near by:"
-      puts "#    Code    Name and Distance"
-      puts "***  *****   ***********************************"
+      puts "   #     Code    Name and Distance"
+      puts " *****   *****   ***********************************"
       airports.each do |a|
-        puts "#{a[:number]}.   (#{a[:code]})   #{a[:name]} is #{a[:distance]} miles away."
+        printf("%5s.   (%3s)   #{a[:name]} is #{a[:distance]} miles away.\n", "#{a[:number]}", "#{a[:code]}" )
         #  puts "#{v}"
       end
     else
@@ -41,10 +41,13 @@ class RunwayFinder::Airport
 
     if runways != nil
       puts "Has the folowing runways."
-      puts "Number  Length  Width  Type   Condition"
-      puts "******  ******  *****  ****   *********"
+      puts "Number     Length    Width    Type       Condition"
+      puts "******  *********  *******    *******    *********"
       runways.each do |r|
-        puts "#{r[:number]}  #{r[:length]}  #{r[:width]}  #{r[:type]} #{r[:condition]}"
+        #puts "#{r[:number]}  #{r[:length]}  #{r[:width]}  #{r[:type]} #{r[:condition]}"
+        printf("%-8s %8s %8s    %-8s   %-10s%\n", "#{r[:number]}", "#{r[:length]}", "#{r[:width]}", "#{r[:type]}", "#{r[:condition]}")
+
+        #{}"  #{r[:width]}  #{r[:type]} #{r[:condition]})
       end
     else
       puts "Cannot find any runways for that airport."
@@ -66,9 +69,9 @@ class RunwayFinder::Airport
     #code = doc.css(".loaddetails")[0].text
 
     doc.css("div#list tr").each_with_index do |row,i|
-      #puts "\n** i:#{i} **\n"
+
       if i > 0 && row.css("td")[0].text != "" && i < 20
-        #puts "\nRow: #{row}:**END OF ROW**"
+
         number = row.css("td")[0].text
 
         code = row.css("td")[1].text
@@ -76,18 +79,9 @@ class RunwayFinder::Airport
         distance = row.css("td")[2].text
         name = row.css("td")[3].text
 
-        #puts "Number:#{number}"
-        #puts "Code is:#{code}"
-        #puts "Distance:#{distance}"
-        #puts "Name:#{name}"
-
         airports << { number: number, code: code, distance: distance, name: name }
       end # if
     end #doc.css("div#list tr").each_with_index do |row,i|
-
-    #puts airports
-    #codeLink = doc.css(".loaddetails").text
-    #distance = doc.css("div#list td")[2].text
 
     # Site will display "Sorry, we could not find any suitable airports for this address!"
     #      if the zip code is not "valid"
@@ -108,28 +102,13 @@ class RunwayFinder::Airport
     runways = []
 
     puts "Searching for details for #{airport_code}"
-    url_with_code = "https://flightaware.com/resources/airport/#{airport_code}/summary"
-    #url_with_code = "http://www.airnav.com/airport/#{airport_code}"
-    #puts "checking site:#{url_with_code}"
+    url_with_code = "http://flightaware.com/resources/airport/K#{airport_code}/summary"
     doc = Nokogiri::HTML( open(url_with_code) )
 
-    #runway_number = doc.css("h4:nth-of-type(3)")
     runway_number = doc.css("div.medium-2.columns tr")
-    #puts "\n*************************"
-    #puts "All:#{runway_number.text}"
-    #puts "***********  After ALL runway numbers **************\n"
 
     runway_number.each_with_index do |r, i|
-      #puts "Row:(#{i}).#{r}"
-      #puts "Checking first cell:#{r.css("td")[0]}"
       if r.css("td")[0].to_s.include?("href")
-        #puts "******   Adding  ********"
-        #puts "Number:#{r.css("td")[0].text}"
-        #puts "Length:#{r.css("td")[1].text}"
-        #puts "Width:#{r.css("td")[2].text}"
-        #puts "Type:#{r.css("td")[3].text}"
-        #puts "Condition:#{r.css("td")[4].text}"
-
         number = r.css("td")[0].text
         length = r.css("td")[1].text
         width = r.css("td")[2].text
@@ -138,8 +117,6 @@ class RunwayFinder::Airport
 
         runways << {number: number, length: length, width: width, type: type, condition: condition}
       end
-      #puts runways
-      #runways
     end
 
     runways
